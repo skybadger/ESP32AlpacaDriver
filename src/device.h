77395ed99b489c3e71dc2e01601f05c51e3d16a6
mike.harrison <mike.harrison@aix.dev>
2026-04-27 16:32:26 +0100

    Aded pinmaps to device.h

---
+++ src/device.h
@@ -0,0 +1,23 @@
+//File to hold common device-specific details. 
+#pragma once
+typedef struct { int pin; char* name } pinmap_t; 
+#if defined WEMOS_D1_MINI32
+pinmap_t pins[] = {
+  { 0, "GPIO0" },
+  { 1, "GPIO1" },
+  { 2, "GPIO2" },
+  { 3, "GPIO3" },
+  { 4, "GPIO4" },
+  { 5, "GPIO5" },
+  { 6, "GPIO6" },
+  { 7, "GPIO7" },
+  { 8, "GPIO8" },
+  { 9, "GPIO9" },
+  {10, "GPIO10"},
+  {11, "GPIO11"},
+  {12, "GPIO12"},
+  {13, "GPIO13"},
+  {14, "GPIO14"},
+  {15, "GPIO15"},
+};
+#endif 
